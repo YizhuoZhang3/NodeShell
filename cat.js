5b2591e75766b709d9e9219af1ca3b33d91fba5b
Zoe Zhang <68212524+YizhuoZhang3@users.noreply.github.com>
2021-09-24 17:08:59 -0400

    Add files via upload

---
+++ cat.js
@@ -0,0 +1,11 @@
+
+/*jshint esversion: 6 */
+const fs = require('fs');
+//
+module.exports = function(file) {
+  fs.readFile(`./${file}`, (err, data) => {
+    if (err) throw err;
+    process.stdout.write(data);
+    process.stdout.write("\nprompt > ");
+  });
+};
