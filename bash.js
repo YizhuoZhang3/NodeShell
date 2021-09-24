cee33f34766eb5a56879efbe1e913a8719b84246
Zoe Zhang <68212524+YizhuoZhang3@users.noreply.github.com>
2021-09-24 15:36:28 -0400

    Add files via upload

---
+++ bash.js
@@ -0,0 +1,13 @@
+process.stdout.write('prompt > ');
+
+// process.stdin.on('data', (data) => {
+//   const cmd = data.toString().trim();
+//   process.stdout.write('You typed: ' + cmd);
+//   process.stdout.write('\nprompt > ');
+// });
+
+const pwdModule = require('./pwd');
+pwdModule();
+
+const ls = require('./ls')
+ls();
