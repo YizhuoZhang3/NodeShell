1ed1a9623d6f2f85442ff789660cd66e235f0542
Zoe Zhang <68212524+YizhuoZhang3@users.noreply.github.com>
2021-09-24 22:14:12 -0400

    Update ls.js

--- ls.js
+++ ls.js
@@ -1,17 +1,11 @@
-const fs = require('fs')
-//
-module.exports = function() {
-    process.stdin.on('data', (data) => {
-      const input = data.toString().trim()
-      if (input === 'ls') {
-        fs.readdir('./', 'utf8', (err, files) => {
-          if (err) {
-            throw err;
-          } else {
-            process.stdout.write(files.join('\n'));
-            process.stdout.write("\nprompt > ");
-          }
-        })
-      }
-    });
-  }
+const fs = require('fs');
+
+module.exports = (done) => {
+  fs.readdir('./', 'utf8', (err, data) => {
+    if (err) {
+      done(err.stack);
+    } else {
+      done(data.join('\n'));
+    }
+  });
+};
