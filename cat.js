46e458c3a27f3cddbd700181ad4182be2f406cdb
Zoe Zhang <68212524+YizhuoZhang3@users.noreply.github.com>
2021-09-24 22:13:44 -0400

    Update cat.js

--- cat.js
+++ cat.js
@@ -1,11 +1,10 @@
-
-/*jshint esversion: 6 */
 const fs = require('fs');
-//
-module.exports = function(file) {
-  fs.readFile(`./${file}`, (err, data) => {
-    if (err) throw err;
-    process.stdout.write(data);
-    process.stdout.write("\nprompt > ");
+module.exports = (fileName, done) => {
+  fs.readFile(fileName, 'utf8', (err, data) => {
+    if (err) {
+      done(err.stack);
+    } else {
+      done(data);
+    }
   });
 };
