5e0ce61a86c33dc12ff23f626919325d96fa7192
Zoe Zhang <68212524+YizhuoZhang3@users.noreply.github.com>
2021-09-24 22:14:33 -0400

    Update pwd.js

--- pwd.js
+++ pwd.js
@@ -1,19 +1,3 @@
-// process.stdout.write('prompt > ');
-// process.stdin.on('data', (data) => {
-  // const cmd = data.toString().trim();
-//   // process.stdout.write('You typed: ' + cmd);
-//   const dire = process.cwd();
-//   process.stdout.write(dire);
-//   process.stdout.write('\nprompt > ');
-// });
-
-module.exports = function() {
-  process.stdin.on('data', (data) => {
-    const input = data.toString().trim();
-    if(input === 'pwd'){
-      const path = process.cwd();
-      process.stdout.write(path);
-      process.stdout.write("\nprompt > ");
-    }
-  });
+module.exports = (done) => {
+  done(process.cwd());
 };
